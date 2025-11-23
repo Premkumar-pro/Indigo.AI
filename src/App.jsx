53c45a456fbdd918284c45cc0da6d04561cfe92a
Premkumar-pro <premkumarponnada777@gmail.com>
2025-11-23 19:32:24 +0530

    remove unwanted code

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,3 @@
-// import React from 'react'
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-// import LandingPage from './pages/LandingPage'
-// import Dashboard from './pages/Dashboard'
-// import ImageGenerator from "./components/dashboard/ImageGenerator";
-
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<LandingPage />} />
-//         <Route path="/dashboard" element={<ImageGenerator />} />
-
-        
-//       </Routes>
-//     </Router>
-//   )
-// }
-
-// export default App
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
@@ -27,7 +7,6 @@ const App = () => {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
-      {/* <Route path="/dashboard" element={<Dashboard />} /> */}
       <Route path="/dashboard/*" element={<Dashboard />} />
     </Routes>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,3 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
-// import './index.css'
-// import { CssBaseline } from '@mui/material'
-// // import { unstable_composeClasses } from '@mui/material';
-// // ...existing code...
-// import { unstable_composeClasses } from '@mui/utils';
-// // quick use to silence linter
-// const _ = unstable_composeClasses;
-// // ...existing code...
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <CssBaseline />
-//     <App />
-//   </React.StrictMode>,
-// )
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
